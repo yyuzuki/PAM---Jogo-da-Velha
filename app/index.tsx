0e31bb3d341a93c060902ab89674d8c1862785dd
yyuzuki <yonamine93@gmail.com>
2025-05-29 10:00:37 -0300

    estilizado

--- app/index.tsx
+++ app/index.tsx
@@ -1,31 +1,84 @@
-import { Text, TouchableOpacity, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
-  const nome = "Yuka";
 
-  return <View>
-    <Text>Jogo da Velha</Text>
-    <Text>Vencedor</Text>
+    const cell = () => {
+        return (
+        <TouchableOpacity style={style.cell}>
+        <Text style={style.cellContent}>X</Text>
+      </TouchableOpacity>
+        )
+    }
 
-    <View>
-        <View>
-            <TouchableOpacity>
-            <Text></Text>
-            </TouchableOpacity>
-        </View>
+  return (
+    <View style={style.container}>
+      <Text style={style.title}>Jogo da Velha</Text>
+      <Text>Vencedor</Text>
 
-        <View>
-        <TouchableOpacity>
-            <Text></Text>
-        </TouchableOpacity>
-        </View>
+      <View>
 
         <View>
-        <TouchableOpacity>
-            <Text></Text>
-        </TouchableOpacity>
+          {cell()}
         </View>
 
-  </View>
-</View>
+        <View style={style.row}>
+          {cell()}
+          {cell()}
+          {cell()}
+        </View>
+
+        <View style={style.row}>
+          {cell()}
+          {cell()}
+          {cell()}
+
+        </View>
+
+        <View style={style.row}>
+        {cell()}
+          {cell()}
+          {cell()}
+
+        </View>
+      </View>
+    </View>
+  );
 }
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: '#ffe5ec',
+    },
+title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+    },
+status: {
+    fontSize: 18,
+    marginBottom: 20,
+},
+board: {
+    marginBottom: 20,
+},
+row: {
+    flexDirection: 'row',
+},
+cell: {
+    width: 80,
+    height: 80,
+    borderWidth: 2,
+    borderColor: '#ffb3c6',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: '#fb6f92',
+},
+cellContent: {
+    fontSize: 36,
+    fontWeight: "bold",
+},
+
+})
